00095a49a1d1322da5d6cbaa5b4705d034529641
Abhi <abhinav118@gmail.com>
2025-08-19 16:31:13 -0400

    Add components/PriorityIcon.tsx

---
+++ components/PriorityIcon.tsx
@@ -0,0 +1,12 @@
+interface PriorityIconProps {
+  size?: any
+  variant?: any
+}
+
+export default function PriorityIcon({ size, variant }: PriorityIconProps) {
+  return (
+    <div className="w-6 h-6 rounded-full flex items-center justify-center">
+      {/* PriorityIcon component content */}
+    </div>
+  )
+}
