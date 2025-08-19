86baa63f62786b4911bae4d7751cc2b944c6414d
Abhi <abhinav118@gmail.com>
2025-08-19 16:31:14 -0400

    Add components/Sidebar.tsx

---
+++ components/Sidebar.tsx
@@ -0,0 +1,12 @@
+interface SidebarProps {
+  collapsible?: any
+  width?: any
+}
+
+export default function Sidebar({ collapsible, width }: SidebarProps) {
+  return (
+    <div className="bg-gray-50 border-r border-gray-200 h-screen">
+      {/* Sidebar component content */}
+    </div>
+  )
+}
