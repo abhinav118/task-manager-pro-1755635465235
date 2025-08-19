75a27afb992f049dc9f7963beee570f51d0759f2
Abhi <abhinav118@gmail.com>
2025-08-19 16:31:14 -0400

    Add components/Navbar.tsx

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+interface NavbarProps {
+  sticky?: any
+  showProfile?: any
+  showNotifications?: any
+}
+
+export default function Navbar({ sticky, showProfile, showNotifications }: NavbarProps) {
+  return (
+    <div className="bg-white border-b border-gray-200 px-6 py-4">
+      {/* Navbar component content */}
+    </div>
+  )
+}
